fcd981dde1b30af7c4db18bfe29728570d43e62e
Kuleshov Dmitry <dkuleshov@lohika.com>
2019-10-18 11:36:48 +0300

    add bitbucket comment

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,14 +1,12 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
-import groovyx.net.http.HTTPBuilder
-import static groovyx.net.http.ContentType
 
 def prComment(String comment) {
-    def http = new HTTPBuilder('https://api.bitbucket.org/2.0/repositories/ClarizenInt')
+    def http = new groovyx.net.http.HTTPBuilder('https://api.bitbucket.org/2.0/repositories/ClarizenInt')
     http.auth.basic(env.bb_user, env.bb_pass)
     http.request(POST) {
         uri.path = '/tf/pullrequests/7/comments'
         body = ['content': ['raw': comment]]
-        requestContentType = ContentType.JSON
+        requestContentType = groovyx.net.http.ContentType.JSON
 
         response.success = { resp ->
             println "Success! ${resp.status}"
