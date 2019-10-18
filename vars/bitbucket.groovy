c3414883e77618439fc2359bcc496ff2115f0a4b
Kuleshov Dmitry <dkuleshov@lohika.com>
2019-10-18 11:56:34 +0300

    add bitbucket comment

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,17 +1,12 @@
 def prComment(String message) {
     withCredentials([usernamePassword(credentialsId: 'bitbucket_http_access', passwordVariable: 'bb_pass', usernameVariable: 'bb_user')]) {
-        try {
-            def baseUrl = new URL("https://api.bitbucket.org/2.0/repositories/ClarizenInt/tf/pullrequests/12/comments").openConnection()
-            def comment = "{'content': {'raw': ${message}}"
-            baseUrl.setRequestMethod("POST")
-            baseUrl.setDoOutput(true)
-            baseUrl.setRequestProperty("Content-Type", "application/json")
-            baseUrl.setRequestProperty("Authorization", "Basic ${bb_user}:${bb_pass}")
-            baseUrl.getOutputStream().write(comment.getBytes("UTF-8"));
-            def postRC = baseUrl.getResponseCode();
-        } catch (ex) {
-            println(ex)
-            println(ex.message)
-        }
+        def baseUrl = new URL("https://api.bitbucket.org/2.0/repositories/ClarizenInt/tf/pullrequests/12/comments").openConnection()
+        def comment = "{'content': {'raw': ${message}}"
+        baseUrl.setRequestMethod("POST")
+        baseUrl.setDoOutput(true)
+        baseUrl.setRequestProperty("Content-Type", "application/json")
+        baseUrl.setRequestProperty("Authorization", "Basic ${bb_user}:${bb_pass}")
+        baseUrl.getOutputStream().write(comment.getBytes("UTF-8"));
+        def postRC = baseUrl.getResponseCode();
     }
 }
