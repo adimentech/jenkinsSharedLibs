dad41a1a47485d0ab4d394077444944eac105101
Kuleshov Dmitry <dkuleshov@lohika.com>
2019-10-18 12:11:09 +0300

    add bitbucket comment

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -2,6 +2,6 @@ def prComment(String message) {
     def data = "{\"content\": {\"raw\": \"${message}\"}}"
     withCredentials([usernameColonPassword(credentialsId: 'bitbucket_http_access', variable: 'bb_credentials')]) {
         String prURL = "https://api.bitbucket.org/2.0/repositories/ClarizenInt/tf/pullrequests/12/comments"
-        sh "curl -X POST -u \${bb_credentials} -H 'Content-Type: application/json' -d '$data' $prURL"
+        sh "curl -s -X POST -u \${bb_credentials} -H 'Content-Type: application/json' -d '$data' $prURL"
     }
 }
