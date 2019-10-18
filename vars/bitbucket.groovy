d5398a97c8a7717ec7b0e6ce7e5dc1fb18690216
Kuleshov Dmitry <dkuleshov@lohika.com>
2019-10-18 11:48:40 +0300

    add bitbucket comment

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,6 +1,6 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
 
-def prComment(String comment) {
+def oldprComment(String comment) {
     def http = new groovyx.net.http.HTTPBuilder('https://api.bitbucket.org/2.0/repositories/ClarizenInt')
     http.auth.basic(env.bb_user, env.bb_pass)
     http.request(groovyx.net.http.Method.POST) {
@@ -14,6 +14,23 @@ def prComment(String comment) {
 
         response.failure = { resp ->
             println "Request failed with status ${resp.status}"
+        }
+    }
+}
+
+def prComment(String message) {
+    withCredentials([usernamePassword(credentialsId: 'bitbucket_http_access', passwordVariable: 'bb_pass', usernameVariable: 'bb_user')]) {
+        def baseUrl = new URL("https://api.bitbucket.org/2.0/repositories/ClarizenInt/tf/pullrequests/7/comments").openConnection()
+        def comment = "{'content': {'raw': ${message}}"
+        baseUrl.setRequestMethod("POST")
+        baseUrl.setDoOutput(true)
+        baseUrl.setRequestProperty("Content-Type", "application/json")
+        baseUrl.setRequestProperty("Authorization", "Basic ${bb_user}:${bb_pass}")
+        baseUrl.getOutputStream().write(comment.getBytes("UTF-8"));
+        try {
+            def postRC = baseUrl.getResponseCode();
+        } catch(ex) {
+            println(ex)
         }
     }
 }
