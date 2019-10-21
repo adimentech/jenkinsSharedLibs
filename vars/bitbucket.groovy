7a4d2707f474731334738d8db5464be266dc7068
Kuleshov Dmitry <dkuleshov@lohika.com>
2019-10-21 11:13:36 +0300

    add bitbucket comment

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,7 +1,14 @@
-def prComment(String message) {
+def prComment(String message, String credentials) {
     def data = "{\"content\": {\"raw\": \"${message}\"}}"
-    withCredentials([usernameColonPassword(credentialsId: 'bitbucket_http_access', variable: 'bb_credentials')]) {
-        String prURL = "https://api.bitbucket.org/2.0/repositories/ClarizenInt/tf/pullrequests/12/comments"
+    //TODO: no error handling in case of unset variables
+    withCredentials([usernameColonPassword(credentialsId: "${credentials}", variable: 'bb_credentials')]) {
+        String prURL = "${getPrApiUrl()}/comments"
         sh "curl -s -X POST -u \${bb_credentials} -H 'Content-Type: application/json' -d '$data' $prURL"
     }
+}
+
+def getPrApiUrl() {
+    def changePath = new URL(env.CHANGE_URL).getPath()
+    def (bbID, bbRepo, rest) = changePath.tokenize("/")
+    return "https://api.bitbucket.org/2.0/repositories/${bbID}/${bbRepo}/pullrequests/${env.CHANGE_ID}"
 }
